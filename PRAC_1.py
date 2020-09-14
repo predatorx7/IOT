1003c04da286510012e68cee48a3b2aee845bffc
predatorx7 <smushaheed@outlook.com>
2020-09-14 11:21:18 +0530

    PRAC 1

---
+++ PRAC_1.py
@@ -0,0 +1,15 @@
+import telepot
+
+token = '1235314909:AAHKiGPZTAJXjFdFCCi-UekQSBRNy_CWH4E'
+
+TelegramBot = telepot.Bot(token)
+
+print(TelegramBot.getMe())
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+    if content_type == 'text':
+        TelegramBot.sendMessage(chat_id, "You said '{}'".format(msg["text"]))
+
+TelegramBot.message_loop(handle)
